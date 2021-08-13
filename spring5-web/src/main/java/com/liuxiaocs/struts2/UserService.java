f02373fb812cbb437ed38e4fb9d958ce85f06456
liuxiao <liuxiao2103@qq.com>
2021-08-14 00:21:30 +0800

    Spring与Strus2的整合

---
+++ spring5-web/src/main/java/com/liuxiaocs/struts2/UserService.java
@@ -0,0 +1,6 @@
+package com.liuxiaocs.struts2;
+
+
+public interface UserService {
+    void register();
+}
---
+++ spring5-web/src/main/java/com/liuxiaocs/struts2/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.liuxiaocs.struts2;
+
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void register() {
+        System.out.println("UserServiceImpl.register");
+    }
+}
