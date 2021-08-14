9cfab8fb9b4a74ffd94223518871a43e47e9c010
liuxiao <liuxiao2103@qq.com>
2021-08-15 00:58:11 +0800

    @ComponentScan包含方式

--- spring5-annotation02/src/main/java/com/liuxiaocs/AppConfig2.java
+++ spring5-annotation02/src/main/java/com/liuxiaocs/AppConfig2.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Service;
 
 @Configuration
+// @ComponentScan(basePackages = "com.liuxiaocs.scan",
+//                excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Service.class}),
+//                                  @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = {"com.liuxiaocs..*"})})
 @ComponentScan(basePackages = "com.liuxiaocs.scan",
-               excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Service.class}),
-                                 @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = {"com.liuxiaocs..*"})})
+               useDefaultFilters = false,
+               includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Service.class})})
 public class AppConfig2 {
 }
